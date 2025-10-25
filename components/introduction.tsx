c62527fccac35dea658fb4c751929081ce38e225
parish <itismmd@gmail.com>
2025-10-25 16:08:31 +0330

    intro for min_w>600px done.

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,13 +5,15 @@ import Image from "next/image";
 export const Introduction = () => {
   return (
     <div
-      className="mx-[88px] h-[486px] bg-[#f4f4f4] flex rounded-3xl flex-1"
+      className="mx-6 sm:mx-10 md:mx-[60px] lg:mx-[88px] h-[486px] bg-[#f4f4f4] flex rounded-3xl"
       dir="rtl"
     >
-      <div className="flex-1 flex items-center justify-center">
-        <div className="w-[622px] flex flex-col gap-6">
-          <h2 className="font-semibold text-[32px]">معرفی Sapway:</h2>
-          <p>
+      <div className="flex-1 flex items-center justify-center min-w-0">
+        <div className="w-full mx-6 min-h-[334px] flex flex-col gap-3">
+          <h2 className="font-semibold ss:text-[clamp(1.063rem,0.394rem+1.784vw,2rem)]">
+            معرفی Sapway:
+          </h2>
+          <p className="ss:text-[clamp(0.875rem,0.607rem+0.713vw,1.25rem)]">
             سپوی یک سامانه هوشمند مدیریت سرویس‌های حمل‌ونقل است که با تکیه بر
             علم داده و هوش مصنوعی، جابجایی افراد در کارخانه ها، مدارس و
             سازمان‌ها را بهینه می‌کند. این سامانه با خوشه‌بندی هوشمند افراد،
@@ -22,7 +24,10 @@ export const Introduction = () => {
             مدیران، رانندگان و مسافران تجربه‌ای یکپارچه، ساده و کارآمد خواهند
             داشت.
           </p>
-          <Button className="w-[135px] h-[46px] text-base font-normal">
+          <Button
+            className="w-[clamp(5.938rem,4.154rem+4.756vw,8.438rem)] h-[clamp(2.019rem,1.408rem+1.629vw,2.875rem)]
+            text-[clamp(0.75rem,0.572rem+0.476vw,1rem)] font-normal"
+          >
             دموی رایگان
           </Button>
         </div>
