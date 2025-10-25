6005f2233dbfe933ce990862510fb6b5131560ff
parish <itismmd@gmail.com>
2025-10-25 17:02:58 +0330

    Added intro for small screen. used clamp for a lot of sizes.added pb-5 to Home page for better display, will be removed later. asjusted the px's and mx's of Hero container and big screen intro section.

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="w-full">
+    <div className="w-full pb-5">
       {/* we don't need flex and flex-col because the divs are natuarlly block elements. */}
       <div className="relative h-auto w-full pb-28 bg-linear-to-r from-[#e2eefe] to-[#f8ceba]">
         <div className="pt-0 ss:pt-[23.5px]">
           <Navbar />
         </div>
-        <div className="px-6 sm:px-[69px] md:px-[74px] lg:px-[88px] mt-0 min-[600px]:mt-[106px]">
+        <div className="px-6 sm:px-8 md:px-[60px] lg:px-[78.5px] mt-0 min-[600px]:mt-[106px]">
           <Hero />
         </div>
       </div>
--- components/introduction.tsx
+++ components/introduction.tsx
@@ -4,16 +4,19 @@ import Image from "next/image";
 
 export const Introduction = () => {
   return (
-    <div
-      className="mx-6 sm:mx-10 md:mx-[60px] lg:mx-[88px] h-[486px] bg-[#f4f4f4] flex rounded-3xl"
-      dir="rtl"
-    >
-      <div className="flex-1 flex items-center justify-center min-w-0">
-        <div className="w-full mx-6 min-h-[334px] flex flex-col gap-3">
-          <h2 className="font-semibold ss:text-[clamp(1.063rem,0.394rem+1.784vw,2rem)]">
-            معرفی Sapway:
-          </h2>
-          <p className="ss:text-[clamp(0.875rem,0.607rem+0.713vw,1.25rem)]">
+    <div dir="rtl">
+      <div className="flex ss:hidden flex-col rounded-xl mx-6">
+        <div className="w-full bg-[#d9d9d9] flex items-center justify-center rounded-t-2xl  min-h-[243px]">
+          <Image
+            src="/icons/play-circle.svg"
+            alt="پخش"
+            width={90}
+            height={90}
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2.5 bg-[#f4f4f4] rounded-b-2xl">
+          <h2 className="font-semibold text-[20px] mt-2.5">معرفی Sapway:</h2>
+          <p className="text-[16px] px-4 pb-4">
             سپوی یک سامانه هوشمند مدیریت سرویس‌های حمل‌ونقل است که با تکیه بر
             علم داده و هوش مصنوعی، جابجایی افراد در کارخانه ها، مدارس و
             سازمان‌ها را بهینه می‌کند. این سامانه با خوشه‌بندی هوشمند افراد،
@@ -24,16 +27,41 @@ export const Introduction = () => {
             مدیران، رانندگان و مسافران تجربه‌ای یکپارچه، ساده و کارآمد خواهند
             داشت.
           </p>
-          <Button
-            className="w-[clamp(5.938rem,4.154rem+4.756vw,8.438rem)] h-[clamp(2.019rem,1.408rem+1.629vw,2.875rem)]
-            text-[clamp(0.75rem,0.572rem+0.476vw,1rem)] font-normal"
-          >
-            دموی رایگان
-          </Button>
         </div>
       </div>
-      <div className="flex-1 bg-[#d9d9d9] flex items-center justify-center rounded-3xl">
-        <Image src="/icons/play-circle.svg" alt="پخش" width={90} height={90} />
+      <div className="mx-6 sm:mx-8 md:mx-[60px] lg:mx-[78.05px] h-[486px] bg-[#f4f4f4] hidden ss:flex rounded-[34px]  ">
+        <div className="flex-1 flex items-center justify-center min-w-0">
+          <div className="w-full mx-6 min-h-[334px] flex flex-col gap-[clamp(1rem,0.643rem+0.951vw,1.5rem)]">
+            <h2 className="font-semibold ss:text-[clamp(1.063rem,0.394rem+1.784vw,2rem)]">
+              معرفی Sapway:
+            </h2>
+            <p className="text-[clamp(0.875rem,0.607rem+0.713vw,1.25rem)]">
+              سپوی یک سامانه هوشمند مدیریت سرویس‌های حمل‌ونقل است که با تکیه بر
+              علم داده و هوش مصنوعی، جابجایی افراد در کارخانه ها، مدارس و
+              سازمان‌ها را بهینه می‌کند. این سامانه با خوشه‌بندی هوشمند افراد،
+              تعیین ایستگاه‌های بهینه و پیشنهاد خودرو مناسب، به کاهش هزینه‌ها و
+              صرفه‌جویی در زمان کمک می‌کند. از سوی دیگر، شرکت‌های حمل‌ونقل
+              می‌توانند ناوگان و تعرفه‌های خود را ثبت کنند تا خدمات‌گیرندگان
+              بهترین انتخاب را داشته باشند. به همراه اپلیکیشن موبایل، همه‌ی
+              مدیران، رانندگان و مسافران تجربه‌ای یکپارچه، ساده و کارآمد خواهند
+              داشت.
+            </p>
+            <Button
+              className="w-[clamp(5.938rem,4.154rem+4.756vw,8.438rem)] h-[clamp(2.019rem,1.408rem+1.629vw,2.875rem)]
+            text-[clamp(0.75rem,0.572rem+0.476vw,1rem)] font-normal"
+            >
+              دموی رایگان
+            </Button>
+          </div>
+        </div>
+        <div className="flex-1 bg-[#d9d9d9] flex items-center justify-center rounded-[34px] ">
+          <Image
+            src="/icons/play-circle.svg"
+            alt="پخش"
+            width={90}
+            height={90}
+          />
+        </div>
       </div>
     </div>
   );
